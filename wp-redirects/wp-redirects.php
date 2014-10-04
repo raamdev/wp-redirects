a14abe867e4cdd380c5974c5b8c1e3d94d72d04d
Raam Dev <raam@websharks-inc.com>
2014-10-04 16:14:13 -0400

    Use WebSharks WP-PHP53 stub to check PHP version requirements

--- wp-redirects/wp-redirects.php
+++ wp-redirects/wp-redirects.php
@@ -13,12 +13,6 @@ Description: Create Redirects! This plugin adds a new Post Type. Redirect from a
 if(!defined('WPINC')) // MUST have WordPress.
 	exit('Do NOT access this file directly: '.basename(__FILE__));
 
-if(version_compare(PHP_VERSION, '5.3', '<'))
-{
-	function wp_redirects_php53_dashboard_notice()
-	{
-		echo __('<div class="error"><p>Plugin NOT active. This version of WP Redirects requires PHP v5.3+.</p></div>', 'wp-redirects');
-	}
-	add_action('all_admin_notices', 'wp_redirects_php53_dashboard_notice');
-}
-else require_once dirname(__FILE__).'/wp-redirects.inc.php';
+if(require(dirname(__FILE__).'/includes/wp-php53.php')) // TRUE if running PHP v5.3+.
+	require_once dirname(__FILE__).'/wp-redirects.inc.php';
+else wp_php53_notice('WP Redirects');
